b3f43b75fcaf5fd880251f9f2ca8ac33dd617fa3
a <a>
2021-10-16 15:24:11 +0700

    update java version, enable lint and caching

--- feature/monster/details/build.gradle.kts
+++ feature/monster/details/build.gradle.kts
@@ -24,11 +24,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
@@ -40,6 +40,9 @@ android {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
+    }
+    lint {
+        checkDependencies = true
     }
 }
 
